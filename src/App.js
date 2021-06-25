d8a3351d72f0f95fa5d2848b924fd6dce4bd52cb
Anakin0101 <mdivani1@mail.com>
2021-06-25 23:11:10 +0400

    route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
-import HomePage from "./pages/homepage/Homepage.component";
+
+import HomePage from "./pages/homepage/Homepage-component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
